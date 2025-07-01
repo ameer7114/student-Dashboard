c9892a478a3379a8373fd9f49d4fea4765a460d4
Ameer Shaik <141486810+ameer7114@users.noreply.github.com>
2025-07-01 13:18:02 +0530

    frontend

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+// src/components/Footer.js
+const Footer = () => {
+  return (
+    <footer style={{ 
+      textAlign: 'center', 
+      color: 'gray', 
+      opacity: 0.5, 
+      padding: '20px 0' 
+    }}>
+      Nueve It Solutions Mini Project
+    </footer>
+  );
+};
+
+export default Footer;
